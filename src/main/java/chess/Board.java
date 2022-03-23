8aa8e46ebb12b4bc942b0cf951b6228dd896fb53
DWL21 <nggus5@naver.com>
2022-03-23 13:21:13 +0900

    refactor: Board 생성자 구조 변경

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,57 +1,33 @@
 package chess;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import chess.piece.*;
 
-import static chess.File.*;
-import static chess.Pieces.*;
-import static chess.Rank.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Board {
-    private static final Map<Position, Pieces> INIT_BOARD = new HashMap<>();
 
-    private final List<Square> squares;
+    private static final String BLANK_SYMBOL = ".";
 
-    static {
-        initBlackPieces();
-        initWhitePieces();
-    }
-
-    private static void initWhitePieces() {
-        INIT_BOARD.put(new Position(ONE, A), WHITE_ROOK);
-        INIT_BOARD.put(new Position(ONE, B), WHITE_KNIGHT);
-        INIT_BOARD.put(new Position(ONE, C), WHITE_BISHOP);
-        INIT_BOARD.put(new Position(ONE, D), WHITE_QUEEN);
-        INIT_BOARD.put(new Position(ONE, E), WHITE_KING);
-        INIT_BOARD.put(new Position(ONE, F), WHITE_BISHOP);
-        INIT_BOARD.put(new Position(ONE, G), WHITE_KNIGHT);
-        INIT_BOARD.put(new Position(ONE, H), WHITE_ROOK);
-        for (File file : File.values()) {
-            INIT_BOARD.put(new Position(TWO, file), WHITE_PAWN);
-        }
-    }
-
-    private static void initBlackPieces() {
-        INIT_BOARD.put(new Position(EIGHT, A), BLACK_ROOK);
-        INIT_BOARD.put(new Position(EIGHT, B), BLACK_KNIGHT);
-        INIT_BOARD.put(new Position(EIGHT, C), BLACK_BISHOP);
-        INIT_BOARD.put(new Position(EIGHT, D), BLACK_QUEEN);
-        INIT_BOARD.put(new Position(EIGHT, E), BLACK_KING);
-        INIT_BOARD.put(new Position(EIGHT, F), BLACK_BISHOP);
-        INIT_BOARD.put(new Position(EIGHT, G), BLACK_KNIGHT);
-        INIT_BOARD.put(new Position(EIGHT, H), BLACK_ROOK);
-        for (File file : File.values()) {
-            INIT_BOARD.put(new Position(SEVEN, file), BLACK_PAWN);
-        }
-    }
+    private final List<Piece> pieces;
 
     public Board() {
-        squares = INIT_BOARD.entrySet()
-                .stream()
-                .map(a -> new Square(a.getKey(), a.getValue())).collect(Collectors.toList());
+        pieces = new ArrayList<>();
+        pieces.addAll(King.getKings());
+        pieces.addAll(Queen.getQueens());
+        pieces.addAll(Rook.getRooks());
+        pieces.addAll(Bishop.getBishops());
+        pieces.addAll(Knight.getKnights());
+        pieces.addAll(Pawn.getPawns());
     }
 
-    public List<Square> getSquares() {
-        return squares;
+    public String findSymbol(Position position){
+        for (Piece piece : pieces) {
+            if (piece.existsOn(position)) {
+                return piece.getSymbol();
+            }
+        }
+        return BLANK_SYMBOL;
     }
 }
